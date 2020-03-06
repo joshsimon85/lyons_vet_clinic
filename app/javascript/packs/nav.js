ca0c01c9d99e25bfe4786b4c61c63e3d3b2a7a07
Josh <joshsimon85@yahoo.com>
2020-03-05 18:20:46 -0700

    Add animations to samll nav

--- app/javascript/packs/nav.js
+++ app/javascript/packs/nav.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const navBar = {
     setNavBarHeight: function() {
       let bounding;
-      this.nav = document.querySelector('.navbar.primary');
+
+      this.nav = document.querySelector('.lg-nav').querySelector('.navbar.primary');
       bounding = this.nav.getBoundingClientRect();
       this.navStartTopHeight = bounding.top;
     },
@@ -15,16 +16,39 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     handleScroll: function(e) {
       let currentHeight;
+
       if (!this.navTimeout) {
         currentHeight = document.documentElement.scrollTop;
         throttle(this.checkScrollHeight, [currentHeight], this, 25, 'navTimeout');
       }
     },
+    displayMenu: function(e) {
+      e.preventDefault();
+
+      this.hamburgerMenu.classList.add('hidden');
+      this.closeIcon.classList.remove('hidden');
+      $(this.menu).slideDown();
+    },
+    hideMenu: function(e) {
+      e.preventDefault();
+
+      this.hamburgerMenu.classList.remove('hidden');
+      this.closeIcon.classList.add('hidden');
+      $(this.menu).slideUp();
+    },
     bindEvents: function() {
       document.addEventListener('scroll', this.handleScroll.bind(this));
+      this.hamburgerMenu.addEventListener('click', this.displayMenu.bind(this));
+      this.closeIcon.addEventListener('click', this.hideMenu.bind(this));
+    },
+    cacheElements: function() {
+      this.hamburgerMenu = document.querySelector('.sm-nav .hamburger_menu');
+      this.closeIcon = document.querySelector('.sm-nav .close');
+      this.menu = document.querySelector('.sm-nav').querySelector('ul');
     },
     init: function() {
       this.setNavBarHeight();
+      this.cacheElements();
       this.bindEvents();
     }
   };
