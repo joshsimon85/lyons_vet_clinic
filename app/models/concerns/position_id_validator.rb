2dd1d7930bdb5f10d45ac04a14a99e477a98b236
Josh <joshsimon85@yahoo.com>
2020-05-13 09:07:10 -0600

    refactor positionidvalidator

--- app/models/concerns/position_id_validator.rb
+++ app/models/concerns/position_id_validator.rb
@@ -1,7 +1,24 @@
 class PositionIdValidator < ActiveModel::Validator
+  attr_reader :privileged_users, :record
+
+  def initialize(record)
+    @privileged_users = ['power user', 'admin']
+    @record = record
+  end
+
   def validate(record)
-    if ['power user', 'admin'].include?(record.role.name.downcase) && record.position == nil
+    if is_privileged_user?(record) && is_position_id_present?(record)
       record.errors.add(:position_id, 'is required for employees')
     end
+  end
+
+  private
+
+  def is_privileged_user?(record)
+    privileged_users.include?(record.role.name.downcase)
+  end
+
+  def is_position_id_present?(record)
+    record.position == nil
   end
 end
