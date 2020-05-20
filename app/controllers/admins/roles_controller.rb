6803c0b6fd5b7a025761ef54c09d725dc2f1b916
Josh <joshsimon85@yahoo.com>
2020-05-19 21:55:20 -0600

    Add tests for admin/role create controller

--- app/controllers/admins/roles_controller.rb
+++ app/controllers/admins/roles_controller.rb
@@ -7,9 +7,25 @@ class Admins::RolesController < ApplicationController
     @roles = Role.all
   end
 
-  def new; end
+  def new
+    @role = Role.new
+  end
 
   def create
+    @role = Role.create(role_params)
 
+    if @role.valid?
+      flash[:success] = 'The role has been successfully created.'
+      redirect_to roles_path
+    else
+      flash.now[:error] = 'There was an error creating the role.'
+      render :new
+    end
+  end
+
+  private
+
+  def role_params
+    params.require(:role).permit(:name, :description, :deletable)
   end
 end
--- spec/controllers/admins/roles_spec.rb
+++ spec/controllers/admins/roles_spec.rb
@@ -32,4 +32,67 @@ RSpec.describe Admins::RolesController do
       end
     end
   end
+
+  describe 'POST create' do
+    it_behaves_like 'requires privileged user' do
+      let(:action) { post :create }
+    end
+
+    context 'with authenticated user' do
+      before { sign_in(jon) }
+
+      context 'with a valid role' do
+        before do
+          post :create, :params => {
+            :role => {
+              :name => 'teting',
+              :description => 'testing description',
+              :deletable => false
+            }
+          }
+        end
+
+        it 'sets a flash success message' do
+          expect(flash[:success]).to be_present
+        end
+
+        it 'redirects to the admin roles index path' do
+          expect(response).to redirect_to(roles_path)
+        end
+
+        it 'saves the new role to the database' do
+          initial_count = Role.count
+          post :create, :params => {
+            :role => {
+              :name => 'teting2',
+              :description => 'testing description',
+              :deletable => false
+              }
+            }
+
+          expect(Role.count).to eq(initial_count + 1)
+        end
+      end
+
+      context 'with an ivalid role' do
+        before do
+          post :create, :params => {
+            :role => {
+              :name => '',
+              :description => '',
+              :deletable => nil
+            }
+          }
+        end
+
+        it 'sets a flash error message' do
+          expect(flash[:error]).to be_present
+        end
+
+        it 'redirects to the admin roles index path' do
+          expect(response).to render_template(:new)
+        end
+      end
+    end
+  end
 end
