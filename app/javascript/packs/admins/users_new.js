25a41a6aca5cf6433fe250a64dfb44b7aa0b9b31
Josh <joshsimon85@yahoo.com>
2020-06-04 10:20:42 -0600

    Add image preview

--- app/javascript/packs/admins/users_new.js
+++ app/javascript/packs/admins/users_new.js
@@ -1,5 +1,7 @@
 import { FormValidator } from '../form_validations';
+import { ImagePreview } from '../image_preview';
 
 document.addEventListener('DOMContentLoaded', () => {
   new FormValidator('new_user').init();
+  new ImagePreview('user_profile_image').init();
 });
--- app/javascript/packs/image_preview.js
+++ app/javascript/packs/image_preview.js
@@ -1,15 +1,31 @@
 class ImagePreview {
   constructor(fileID) {
-    this.fileId = fileID;
+    this.fileInput = document.getElementById(fileID);
+    this.imagePreview = document.querySelector('.image-preview');
   }
-  addPreview() {
-    
+  readImage(e) {
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    const self = this;
+
+    if (file.type.indexOf('image') === -1) {
+      console.log('The file is not an image');
+      return
+    }
+
+    reader.addEventListener('load', (e) => {
+      self.imagePreview.src = e.target.result;
+      self.imagePreview.closest('.hidden').classList.remove('hidden');
+    });
+
+    reader.readAsDataURL(file)
   }
   bindEvents() {
-    const fileInput = document.getElementById(this.fileID);
-    fileInput.addEventListener('change', this.addPreview.bind(this));
+    this.fileInput.addEventListener('change', this.readImage.bind(this));
   }
   init() {
     this.bindEvents();
   }
 }
+
+export { ImagePreview }
