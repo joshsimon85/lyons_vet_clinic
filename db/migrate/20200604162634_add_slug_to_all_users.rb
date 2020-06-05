37ac10c6109350be77763ea89bf163177789356e
Josh <joshsimon85@yahoo.com>
2020-06-05 06:55:46 -0600

    Add slugs to users in system

--- db/migrate/20200604162634_add_slug_to_all_users.rb
+++ db/migrate/20200604162634_add_slug_to_all_users.rb
@@ -1,7 +1,7 @@
 class AddSlugToAllUsers < ActiveRecord::Migration[6.0]
   def change
     User.all.each do |user|
-      user.update_attribute(:slug, user.full_name.downcase.parameterize)
-    end 
+      user.update(:updated_at => Time.now)
+    end
   end
 end
