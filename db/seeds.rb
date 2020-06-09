35c367b2e9c51a2c4d56326a1d370c6ba05d3ccc
Josh <joshsimon85@yahoo.com>
2020-06-09 14:30:27 -0600

    Add seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,32 +11,8 @@ external_user = Role.create({ :name => 'user',
                      :description => 'External customer',
                      :deletable => false })
 
-vet_tech = Position.create({ :name => 'Vet tech', :description => 'Vet tech'} )
-
 User.create({ :full_name => 'Jon Doe',
                            :email => 'jon@doe.com',
                            :password => 'password',
                            :confirmed_at => Time.now,
                            :role =>  power_user })
-
-User.create({ :full_name => 'Admin User',
-                           :email => 'admin@user.com',
-                           :password => 'password',
-                           :confirmed_at => Time.now,
-                           :role => admin,
-                           :position => vet_tech,
-                           :description => 'Fun person' })
-
-User.create({ :full_name => 'Jane Doe',
-                         :email => 'jane@doe.com',
-                         :password => 'password',
-                         :confirmed_at => Time.now,
-                         :role => employee,
-                         :position => vet_tech,
-                         :description => 'Fun employee' })
-
-User.create({ :full_name => 'External User',
-                     :email => 'user@user.com',
-                     :password => 'password',
-                     :confirmed_at => Time.now,
-                     :role => external_user })
